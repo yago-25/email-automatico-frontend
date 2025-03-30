ce52d7a130d8424443090c7a2b8142ed8edeca81
vitorhugouau <vitorhugkrodrigues@gmail.com>
2025-03-30 17:37:20 -0300

    feat: send reset password

--- src/pages/ResetPassword/EmailVerification.tsx
+++ src/pages/ResetPassword/EmailVerification.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";  // Importar o useEffect junto com useState
+import { useState, useEffect } from "react";  // Importar o useEffect junto com useState
 import { useNavigate, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./EmailVerification.css";
