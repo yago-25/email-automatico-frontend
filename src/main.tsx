39b42ab88abed84dc560043faacfd6dbcb686298
yagofontanez <yagofontanez20@icloud.com>
2025-04-21 19:28:55 -0300

    fix: remove ,

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,6 @@ createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId="196292942822-dit8g1hkfhnkhnk4ip66i046nco4shnr.apps.googleusercontent.com">
     <StrictMode>
       <App />
-    </StrictMode>,
+    </StrictMode>
   </GoogleOAuthProvider>
 )
